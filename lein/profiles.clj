ae5b95b48b60ffa0ef2e512ac5588df1ffdb89ce
Simon Génier <simon.genier@shopify.com>
2015-09-28 21:10:33 -0400

    clj: turn off tiered compilation.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,4 @@
 {:user {:plugins [[cider/cider-nrepl "0.9.1"]
                   [refactor-nrepl "1.1.0"]
-                  [lein-ancient "0.6.7"]]}}
+                  [lein-ancient "0.6.7"]]
+        :jvm-opts ^:replace []}}
